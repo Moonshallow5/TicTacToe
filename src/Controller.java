dd4fdfff74052bf415bbf2e1a8e1784b54163b2f
Sandeep Singh <ssandeepssingh55@gmail.com>
2021-04-24 00:08:19 +0800

    Changed a lot

--- src/Controller.java
+++ src/Controller.java
@@ -13,21 +13,25 @@ public final class Controller {
     public void startSession() {
         view.initialDisplay(model);
         view.displayBoard(model);
-        while (turn<=9 ) {
+        while (turn<=2 || !model.Winner) {
             move();
             turn++;
             view.switchPlayers(model);
+
+        if(model.Winner || turn>2) {
+            break;
+        }
         }
     }
     public void move(){
         int rowMove=view.makeMoveRow();
         int columnMove=view.makeMoveColumn();
-        validPlay=model.isMoveValid(rowMove,columnMove);
+        validPlay=model.isMoveValid(rowMove-1,columnMove-1);
         while(!validPlay){
             System.out.println("again");
             rowMove=view.makeMoveRow();
             columnMove=view.makeMoveColumn();
-            validPlay=model.isMoveValid(rowMove,columnMove);
+            validPlay=model.isMoveValid(rowMove-1,columnMove-1);
         }
         model.makeMove(rowMove-1,columnMove-1);
         view.displayBoard(model);
--- src/Model.java
+++ src/Model.java
@@ -1,5 +1,6 @@
 public final class Model {
     char player='X';
+    boolean Winner=false;
 
     public char[][] board;
     public Model() {
@@ -9,19 +10,58 @@ public final class Model {
         // Initialise the board size to its default values.
 }
     public boolean isMoveValid(int x,int y){
-        if((x>3||x<0) || (y>3||y<0) ){
+        if((x>3||x<0) || (y>3||y<0) || board[x][y]!=' ' ){
+            System.out.println("ed");
             return false;
 
         }return true;
     }
     public void makeMove(int x,int y){
       boolean validPlay=isMoveValid(x,y);
+      if (board[x][y]!=' '){
+          System.out.println("Not valid");
 
-        if(board[x][y]==' '){
+      }
+
+        else if(board[x][y]==' '){
             board[x][y]=player;
         }
+            Winner=isWinnerHorizontal(player,board)||isWinnerVertical(player,board);
+
+    }
+    public boolean isWinnerHorizontal(char player,char[][] board){
+        for (int row = 0; row < board.length; row++) {
+            int rowCount=0;
+            for (int col = 0; col < board[0].length; col++) {
+                if(board[row][col]==player){
+                    rowCount++;
+                }else {
+                    rowCount=0;
+                }if(rowCount>=3){
+                    System.out.println("cool");
+                    return true;
+                }
+
+            }
+
+        }return false;
+    }
+    public boolean isWinnerVertical(char player,char[][] board){
+        for (int col = 0; col < board[0].length; col++) {
+            int columnCount=0;
+            for (int row = 0; row < board.length; row++) {
+                if(board[row][col]==player){
+                    columnCount++;
+                }else {
+                    columnCount=0;
+                }if(columnCount>=3){
+                    return true;
+                }
 
 
+            }
+
+        }return false;
     }
 
 }
